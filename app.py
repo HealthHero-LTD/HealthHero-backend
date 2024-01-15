266efa83ef0d9f0c67515ae0a56f8abb6bda1c3f
Soroush Kami <soroushkami10@gmail.com>
2024-01-15 06:02:13 -0500

    implement postgres and connect to backend

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
-
+import os
+import psycopg2
 from flask import Flask, request, jsonify
+
+# db configs
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+
+DATABASE_URL = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@localhost:5432/{DB_NAME}"
+)
 
 app = Flask(__name__)
 
@@ -15,7 +26,15 @@ def index_get():
 def index_post():
     data = request.get_json()
     print(f"received data: {data}")
-    return jsonify({'message': 'data transferred!'})
+    
+    try:
+        dbConnection = psycopg2.connect(DATABASE_URL)
+        print("connected to the db successfully")
+        return jsonify({'message': 'data transferred!'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    finally:
+        dbConnection.close()
     
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=6969, debug=True)
