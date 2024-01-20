277d521577ef5deef8326c87cd6b03394fff7fe3
Soroush Kami <soroushkami10@gmail.com>
2024-01-20 14:39:01 -0500

    create sql_queries.py

---
+++ sql_queries.py
@@ -0,0 +1,19 @@
+create_user_table_query = """
+CREATE TABLE IF NOT EXISTS "user-profile" (
+    username VARCHAR(50) PRIMARY KEY,
+    google_email_address VARCHAR(50) UNIQUE,
+    steps INTEGER,
+    token VARCHAR(256)
+);
+"""
+
+insert_user_query = """
+INSERT INTO "user-profile" (username, google_email_address, steps)
+VALUES (%s, %s, %s);
+"""
+
+update_token_query = """
+UPDATE "user-profile"
+SET token = %s
+WHERE username = %s;
+"""
